f863529985bad17580af4942a95a4ad816052de1
lekhanh2099 <lekhanh2099@gmail.com>
2021-01-06 22:32:10 +0700

    delete function err

--- For_homework/bai-2/main.js
+++ For_homework/bai-2/main.js
@@ -19,34 +19,3 @@ function handleEx_2(x, n) {
     };
     return sum + x;
 };
-
-let input_1 = FORM_2.querySelector("#value_ex_2-1");
-let input_2 = FORM_2.querySelector("#value_ex_2-2");
-function validate(x, y) {
-    let errNotice_1 = document.querySelector("#err_2-1");
-    let errNotice_2 = document.querySelector("#err_2-2");
-    x.onblur = function () {
-        if (x.value == "") {
-            if (x.id === "value_ex_2-1") {
-                errNotice_1.innerHTML = "Đây là thông báo lỗi input 1";
-            }
-        }
-    }
-    y.onblur = function () {
-        if (y.value == "") {
-            if (y.id === "value_ex_2-2") {
-                errNotice_2.innerHTML = "Đây là thông báo lỗi input 2";
-            }
-        }
-    }
-    x.oninput = function () {
-        errNotice_1.innerHTML = "";
-    }
-
-    y.oninput = function () {
-        errNotice_2.innerHTML = "";
-
-    }
-};
-
-validate(input_1, input_2);
